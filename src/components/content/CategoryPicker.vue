b5b69c2eedce51390c43e90f20ae243a7a0f8184
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-09 16:54:55 -0300

    feat: add base input

--- src/components/content/CategoryPicker.vue
+++ src/components/content/CategoryPicker.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import SectionHeading from '@components/ui/SectionHeading.vue'
 import BaseCard from '@components/ui/BaseCard.vue'
+import BaseInput from '@components/ui/BaseInput.vue'
 </script>
 
 <template>
@@ -8,8 +9,9 @@ import BaseCard from '@components/ui/BaseCard.vue'
     <SectionHeading type="h4" title="Categories" />
     <BaseCard>
       <div>
-        <div>
+        <div class="flex flex-col gap-4">
           <SectionHeading type="h5" title="Valor ideal" />
+          <BaseInput placeholder="R$ 0" />
         </div>
         <div>
           <div>
