0d26d64ed432bdf2f71e131cb2c10416087073bc
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-09 16:30:00 -0300

    fix: card padding highlight

--- src/components/HighlightData.vue
+++ src/components/HighlightData.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import Card from '@/components/ui/BaseCard.vue'
+import BaseCard from '@/components/ui/BaseCard.vue'
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
-    <Card title="Saldo disponível" type="highlight" data="R$12.529,24">testes</Card>
-    <Card title="Visualizações de perfil" data="24">teste</Card>
-    <Card title="Avaliações gerais" data="4.9">testes</Card>
-    <Card title="Suas conquistas"></Card>
+    <BaseCard title="Saldo disponível" type="highlight" gradient data="R$12.529,24"
+      >testes</BaseCard
+    >
+    <BaseCard title="Visualizações de perfil" type="highlight" data="24">teste</BaseCard>
+    <BaseCard title="Avaliações gerais" type="highlight" data="4.9">testes</BaseCard>
+    <BaseCard title="Suas conquistas" type="highlight"></BaseCard>
   </div>
 </template>
--- src/components/ui/BaseCard.vue
+++ src/components/ui/BaseCard.vue
@@ -1,33 +1,38 @@
 <script setup lang="ts">
+import gradientPath from '@assets/gradient-path.png'
+
 const props = withDefaults(
   defineProps<{
     type?: 'highlight' | 'default'
+    gradient?: boolean
     title: string
     data?: string
   }>(),
   {
     type: 'default',
+    gradient: false,
   },
 )
 </script>
 
 <template>
   <div
-    class="relative rounded-lg py-8 px-7 overflow-hidden"
+    class="relative rounded-lg overflow-hidden border-1 border-neutral-gray shadow-[0px_13px_18px_0px_#3E00560D]"
     :class="[
-      props.type === 'default'
-        ? 'border-1 border-neutral-gray shadow-[0px_13px_18px_0px_#3E00560D]'
-        : `bg-linear-270 from-[#EE7696] via-[#8A05BE] to-[#EE7696]`,
+      props.type === 'default' ? 'p-6' : `py-8 px-7`,
+      props.gradient &&
+        'bg-linear-270 from-[#EE7696] via-[#8A05BE] to-[#EE7696] border-none shadow-none',
     ]"
   >
     <img
-      v-if="props.type === 'highlight'"
-      src="../assets/gradient-path.png"
+      v-if="props.gradient"
+      :src="gradientPath"
       alt="Glow"
       class="absolute -top-12 left-0 z-0 pointer-events-none"
     />
 
     <div class="relative z-10 grid grid-cols-1 gap-5">
+      {{ props.gradient }}
       <h3 class="font-medium text-neutral-purple">{{ props.title }}</h3>
       <p v-if="props.data" class="text-2xl font-semibold text-secondary">
         {{ props.data }}
