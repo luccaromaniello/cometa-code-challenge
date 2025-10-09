90225f43352e06d82de46a296672ca8674aeb685
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-09 18:25:01 -0300

    feat: start structure for jobpicker

--- src/components/sections/ContentData.vue
+++ src/components/sections/ContentData.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import CategoryPicker from '@components/content/CategoryPicker.vue'
+import JobPicker from '@components/content/JobPicker.vue'
 </script>
 
 <template>
   <div class="grid grid-cols-[1fr_1.8fr_1.2fr] gap-5">
     <CategoryPicker />
-    <CategoryPicker />
+    <JobPicker />
     <CategoryPicker />
   </div>
 </template>
