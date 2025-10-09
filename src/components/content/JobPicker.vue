3d652b6d4e2ab2fe30d90c2293165d485ba3c57c
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-09 20:54:34 -0300

    feat: add base for jobpicker

---
+++ src/components/content/JobPicker.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import SectionHeading from '@components/ui/SectionHeading.vue'
+import BaseCard from '@components/ui/BaseCard.vue'
+import BaseInput from '@components/ui/BaseInput.vue'
+</script>
+
+<template>
+  <div class="grid grid-cols-1 gap-7">
+    <SectionHeading type="h4" title="Encontre o trabalho ideal" />
+    <BaseInput type="text" placeholder="R$ 0" />
+    <BaseCard>teste</BaseCard>
+  </div>
+</template>
