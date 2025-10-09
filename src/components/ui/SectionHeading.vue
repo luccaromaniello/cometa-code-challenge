2565e58bdf95fceaf80e56f36dfdcfe11021f068
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-09 16:44:08 -0300

    refactor: heading props

--- src/components/ui/SectionHeading.vue
+++ src/components/ui/SectionHeading.vue
@@ -10,14 +10,12 @@ const props = defineProps<{
     :is="props.type"
     :class="[
       'text-neutral-purple font-semibold',
-      {
-        'text-4xl': props.type === 'h1',
-        'text-3xl': props.type === 'h2',
-        'text-2xl': props.type === 'h3',
-        'text-xl': props.type === 'h4',
-        'text-lg': props.type === 'h5',
-        'text-base': props.type === 'h6',
-      },
+      props.type === 'h1' && 'text-4xl',
+      props.type === 'h2' && 'text-3xl',
+      props.type === 'h3' && 'text-2xl',
+      props.type === 'h4' && 'text-xl',
+      props.type === 'h5' && 'text-base',
+      props.type === 'h6' && 'text-base',
     ]"
   >
     {{ props.title }}
