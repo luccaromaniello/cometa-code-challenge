2c5cd8b953ed9cf36716fa48e580e2d52a71d1af
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-09 17:02:18 -0300

    style: add search icon

--- src/components/ui/BaseInput.vue
+++ src/components/ui/BaseInput.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
+import searchIcon from '@assets/icons/search.svg'
 
 const props = defineProps<{
   modelValue: string
@@ -31,14 +32,21 @@ watch(modelValue, (val) => {
 
 <template>
   <div class="flex flex-col gap-1">
-    <input
-      :id="id"
-      :type="type"
-      v-model="modelValue"
-      :placeholder="placeholder"
-      :disabled="disabled"
-      class="px-4 py-2 border border-neutral-gray rounded-md text-sm text-neutral-800 focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent disabled:bg-neutral-100 disabled:cursor-not-allowed placeholder-neutral-dark-grey"
-    />
+    <div class="relative">
+      <input
+        :id="id"
+        :type="type"
+        v-model="modelValue"
+        :placeholder="placeholder"
+        :disabled="disabled"
+        class="w-full px-4 py-2 border border-neutral-gray rounded-md text-sm text-neutral-800 focus:outline-none focus:ring-1 focus:ring-primary focus:border-transparent disabled:bg-neutral-100 disabled:cursor-not-allowed placeholder-neutral-dark-grey"
+      />
+      <img
+        :src="searchIcon"
+        alt="Search icon"
+        class="w-5 h-5 absolute top-1/2 right-4 -translate-y-1/2"
+      />
+    </div>
 
     <p v-if="error" class="text-sm text-red-500 mt-1">{{ error }}</p>
   </div>
