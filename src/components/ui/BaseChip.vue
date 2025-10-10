6baa5e2f32359af7cc7e6e20d4898e6192bf7c7c
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-10 14:07:58 -0300

    fix: removed hihglight chip border

--- src/components/ui/BaseChip.vue
+++ src/components/ui/BaseChip.vue
@@ -16,25 +16,33 @@ const props = withDefaults(
 
 <template>
   <div
-    :class="[
-      'relative group text-sm font-medium px-4 py-2 rounded-full whitespace-nowrap cursor-default flex items-center',
-      props.type === 'highlight'
-        ? 'p-[1px] bg-gradient-to-r from-[#8A05BE] to-[#EE7696]'
-        : 'bg-neutral-light-gray text-black',
-    ]"
+    v-if="props.type === 'highlight'"
+    class="p-[1px] rounded-full bg-gradient-to-r from-[#8A05BE] to-[#EE7696]"
   >
-    <template v-if="props.type === 'highlight'">
+    <div
+      class="relative group text-sm font-medium px-4 py-2 rounded-full whitespace-nowrap cursor-default flex items-center gap-2 bg-linear-120 from-[#31283D] to-[#371503]"
+    >
       <div class="w-3 h-4 flex-shrink-0">
         <img :src="fireEmoji" alt="Fire emoji" />
       </div>
       <span class="bg-gradient-to-r from-[#EE7696] to-[#F5C32C] bg-clip-text text-transparent">
         {{ props.label }}
       </span>
-    </template>
 
-    <template v-else>
-      {{ props.label }}
-    </template>
+      <div
+        v-if="props.tooltip?.length"
+        class="absolute z-10 top-full left-1/2 -translate-x-1/2 mt-1 hidden group-hover:block bg-gray-900 text-white text-xs rounded px-2 py-1 whitespace-nowrap shadow-lg pointer-events-none"
+      >
+        {{ props.tooltip.join(', ') }}
+      </div>
+    </div>
+  </div>
+
+  <div
+    v-else
+    class="relative group text-sm font-medium px-4 py-2 rounded-full whitespace-nowrap cursor-default flex items-center bg-neutral-light-gray text-black"
+  >
+    {{ props.label }}
 
     <div
       v-if="props.tooltip?.length"
