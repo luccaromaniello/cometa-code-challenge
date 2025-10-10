c1d6f525e7b306176382c6bc5dc8ed123c2325b2
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-10 18:01:21 -0300

    feat: add navbar

--- src/data/icons.ts
+++ src/data/icons.ts
@@ -1,9 +1,11 @@
+import bell from '@assets/icons/bell.svg'
 import calendar from '@assets/icons/calendar.svg'
 import heart from '@assets/icons/heart.svg'
 import search from '@assets/icons/search.svg'
 import wallet from '@assets/icons/wallet.svg'
 
 export const iconMap = {
+  bell,
   calendar,
   heart,
   search,
@@ -11,6 +13,7 @@ export const iconMap = {
 } as const
 
 export const iconAltMap: Record<keyof typeof iconMap, string> = {
+  bell: 'Bell icon',
   calendar: 'Calendar icon',
   heart: 'Heart icon',
   search: 'Search icon',
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import Highlights from '@components/sections/HighlightData.vue'
 import Content from '@components/sections/ContentData.vue'
+import NavBar from '@/components/navigation/NavBar.vue'
 </script>
 
 <template>
-  <div class="min-h-screen flex flex-col p-4 w-full rounded-t-xl bg-white text-neutral-purple">
-    <!-- TODO: nav -->
+  <div
+    class="min-h-screen flex flex-col gap-12 py-5 px-4 w-full rounded-t-xl bg-white text-neutral-purple"
+  >
+    <NavBar />
     <main class="grid grid-cols-1 gap-20">
       <Highlights />
       <Content />
