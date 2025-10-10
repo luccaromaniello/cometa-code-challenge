516bb10dca49b1662c587d19391e0b7635c634d6
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-10 20:59:07 -0300

    feat: add search to jobs

--- src/components/content/JobPicker.vue
+++ src/components/content/JobPicker.vue
@@ -1,13 +1,16 @@
 <script setup lang="ts">
+import { ref } from 'vue'
 import SectionHeading from '@components/ui/SectionHeading.vue'
 import BaseInput from '@components/ui/BaseInput.vue'
 import JobList from '@components/content/JobList.vue'
+
+const search = ref('')
 </script>
 
 <template>
   <div class="flex flex-col gap-7">
     <SectionHeading type="h4" title="Encontre o trabalho ideal" />
-    <BaseInput type="text" placeholder="Buscar" />
-    <JobList />
+    <BaseInput v-model="search" type="text" placeholder="Buscar" />
+    <JobList :search="search" />
   </div>
 </template>
