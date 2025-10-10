32e4d17cca7a36c16c64e29ba30c4b9ee70da217
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-10 15:01:20 -0300

    feat: add badges

--- src/components/sections/HighlightData.vue
+++ src/components/sections/HighlightData.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
-import BaseCard from '@/components/ui/BaseCard.vue'
+import BaseCard from '@components/ui/BaseCard.vue'
+import zupiBadge from '@assets/icons/badges/zupi.svg'
+import founderBadge from '@assets/icons/badges/founder.svg'
 </script>
 
 <template>
@@ -9,6 +11,11 @@ import BaseCard from '@/components/ui/BaseCard.vue'
     >
     <BaseCard title="Visualizações de perfil" type="highlight" data="24">teste</BaseCard>
     <BaseCard title="Avaliações gerais" type="highlight" data="4.9">testes</BaseCard>
-    <BaseCard title="Suas conquistas" type="highlight"></BaseCard>
+    <BaseCard title="Suas conquistas" type="highlight">
+      <div class="flex gap-3 -ml-2 h-14">
+        <img :src="zupiBadge" alt="Zupi Badge" />
+        <img :src="founderBadge" alt="Founder Badge" />
+      </div>
+    </BaseCard>
   </div>
 </template>
