bcc3ebc67325f87b973cd48577bdb9e945c03933
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2025-10-10 14:34:59 -0300

    feat: add icon buttons

---
+++ src/components/ui/IconButton.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { defineProps, computed } from 'vue'
+import { iconMap, iconAltMap } from '@data/icons'
+
+const props = defineProps<{
+  icon: keyof typeof iconMap
+}>()
+
+const emit = defineEmits<{
+  (e: 'click', event: MouseEvent): void
+}>()
+
+const iconSrc = computed(() => iconMap[props.icon])
+const altText = computed(() => iconAltMap[props.icon])
+
+function handleClick(event: MouseEvent) {
+  emit('click', event)
+}
+</script>
+
+<template>
+  <button
+    @click="handleClick"
+    class="flex items-center justify-center rounded-full border border-neutral-light-gray bg-white hover:bg-neutral-light-gray/20 transition-colors w-full h-full min-w-[20px] min-h-[20px]"
+  >
+    <img :src="iconSrc" :alt="altText" class="h-full w-auto object-contain p-2" />
+  </button>
+</template>
